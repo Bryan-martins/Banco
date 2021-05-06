774cc7087a1bd6d81e1642618ce169b9eecbb39b
Bryan-martins <ridangamer@gmail.com>
2021-05-06 19:42:02 -0300

    Modificando alguns métodos

--- Classes2Q.py
+++ Classes2Q.py
@@ -1,55 +1,13 @@
 class Conta:
 
     _total_contas = 0
-    __slots__ = ['_numero', '_titular', 'saldo', '_limite', '_historico']
 
-    def __init__(self, numero, cliente, saldo, limite = 10000):
-        self._numero = numero
-        self._titular = cliente
-        self.saldo = saldo
-        self._limite = limite
-        self._historico = Historico()
-        Conta._total_contas += 1
+    def __init__(self):
         self._contas = []
-
-    @staticmethod
-    def get_total_contas():
-        return Conta._total_contas
-
-    @property
-    def numero(self):
-        return self._numero
-
-    @numero.setter
-    def numero(self, numero):
-        self._numero = numero
-    
-    @property
-    def titular(self):
-        return self._titular
-
-    @titular.setter
-    def titular(self, titular):
-        self._titular = titular
-
-    @property
-    def limite(self):
-        return self._limite
-
-    @limite.setter
-    def limite(self, limite):
-        self._limite = limite
-    
-    @property
-    def historico(self):
-        return self._historico
-
-    @historico.setter
-    def historico(self, historico):
-        self._historico = historico
+        Conta._total_contas += 1
 
     def cadastra(self, pessoa):
-        existe = self.busca(pessoa.titular.cpf)
+        existe = self.busca(pessoa.numero, pessoa.cpf)
         if(existe == None):
             self._contas.append(pessoa)
             return True
@@ -57,16 +15,9 @@ class Conta:
         else:
             return False
 
-    '''def login(self):
-        numero = input('Digite o numero da conta: ')
-        cpf = input('Digite o CPF da conta: ')
+    def busca(self, numero, cpf):
         for x in self._contas:
-            if numero == x.numero and cpf == x.titular.cpf:
-    '''
-
-    def busca(self, cpf):
-        for x in self._contas:
-            if x.titular.cpf == cpf:
+            if x.cpf == cpf and x.numero == numero:
                 return x
         
         return None
@@ -87,81 +38,39 @@ class Conta:
             self.historico.mov.append('Saque de R$ {}'.format(valor)) 
             return True
         else:
-            print('Sem saldo! ')
             return False
     
     def extrato(self):
         print('Saldo: {} \nConta: {}'.format(self.saldo, self.numero))
         self.historico.mov.append('Tirado o extrato!, saldo de R$ {}'.format(self.saldo))
     
-    def transfere(self, cpf):
-        opc = int(input('Digite a conta para qual vai transferir: '))
-
+    def transfere(self, numero, valor, pessoa):
         for x in self._contas:
-            if x.numero == opc:     
-                res = self.saca(valor)
+            if x.numero == numero:     
+                res = pessoa.saca(valor)
                 if res:
                     res2 = x.deposita(valor)
                     if res2:
-                        self.historico.mov.append('Tranferencia para a conta {}, no valor de R$ {} (Saque acima)'.format(x.numero,valor))
+                        pessoa.historico.mov.append('Tranferencia para a conta {}, no valor de R$ {} (Saque acima)'.format(x.numero,valor))
                     
                     else:
-                        self.deposita(valor)
+                        pessoa.deposita(valor)
                 else:
                     return False
-    '''
-    def menu(self, numero):
-        cont = 1
 
-        while cont != 0:
-            print('MENU: ')
-            print('1 - Depositar: ')
-            print('2 - Sacar: ')
-            print('3 - Extrato: ')
-            print('4 - Transferir: ')
-            print('5 - Historico: ')
-            print('0 - Sair: ')
-            opc = int(input())
-
-            if opc == 1:
-                valor = float(input('Digite o valor: '))
-                self.deposita(valor)
-                print()
-            
-            elif opc == 2:
-                valor = float(input('Digite o valor: '))
-                self.saca(valor)
-                print()
-
-            elif opc == 3:
-                self.extrato()
-                print()
-            
-            elif opc == 4:
-                valor = float(input('Digite o valor: '))
-                self.transfere(contas, valor)
-                print()
-            
-            elif opc == 5:
-                print('Abertura da conta:', self.historico.abertura)
-                for x in range(len(self.historico.mov)):
-                    print('-', self.historico.mov[x])
-                
-                print()
-        
-            elif opc == 0:
-                cont = 0
-
-            else:
-                print('Digite uma opção válida! ')
-                print()
-    '''
 
 class Cliente:
-    def __init__(self, nome, sn, cpf):
+
+    __slots__ = ['_nome','_numero', '_sn','_cpf', 'saldo', '_limite', '_historico']
+
+    def __init__(self, numero, nome, sn, cpf, saldo, limite = 10000):
+        self._numero = numero
         self._nome = nome
         self._sn = sn
         self._cpf = cpf
+        self.saldo = saldo
+        self._limite = limite
+        self._historico = Historico()
 
     @property
     def nome(self):
@@ -186,6 +95,34 @@ class Cliente:
     @cpf.setter
     def cpf(self, cpf):
         self._cpf = cpf
+
+    @staticmethod
+    def get_total_contas():
+        return Conta._total_contas
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @numero.setter
+    def numero(self, numero):
+        self._numero = numero
+
+    @property
+    def limite(self):
+        return self._limite
+
+    @limite.setter
+    def limite(self, limite):
+        self._limite = limite
+    
+    @property
+    def historico(self):
+        return self._historico
+
+    @historico.setter
+    def historico(self, historico):
+        self._historico = historico
 
 import datetime
 

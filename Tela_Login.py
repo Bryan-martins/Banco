a96453f7cdf4325adce14b3aeca9fa3c4c9d0a13
Bryan-martins <ridangamer@gmail.com>
2021-05-06 19:44:52 -0300

    Modificando as telas

--- Tela_Login.py
+++ Tela_Login.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Tela_Transferir.ui'
+# Form implementation generated from reading ui file 'Tela_Login.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
@@ -11,53 +11,43 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Tela_transferir(object):
+class Tela_login(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(640, 480)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(40, 40, 241, 30))
+        self.label.setGeometry(QtCore.QRect(140, 170, 141, 21))
         font = QtGui.QFont()
-        font.setPointSize(20)
+        font.setPointSize(12)
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(40, 110, 141, 21))
+        self.label_2.setGeometry(QtCore.QRect(140, 250, 81, 16))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(40, 150, 51, 21))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(40, 350, 231, 31))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.label_4.setFont(font)
-        self.label_4.setObjectName("label_4")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(300, 110, 141, 20))
+        self.lineEdit.setGeometry(QtCore.QRect(290, 170, 191, 20))
         self.lineEdit.setObjectName("lineEdit")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(300, 150, 141, 20))
+        self.lineEdit_2.setGeometry(QtCore.QRect(290, 250, 191, 20))
         self.lineEdit_2.setObjectName("lineEdit_2")
-        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_3.setGeometry(QtCore.QRect(300, 350, 141, 31))
-        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(250, 70, 91, 31))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(540, 390, 80, 30))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(250, 330, 80, 30))
+        self.pushButton_2.setObjectName("pushButton_2")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -68,18 +58,18 @@ class Tela_transferir(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Transferência para:"))
-        self.label_2.setText(_translate("MainWindow", "Número da conta: "))
-        self.label_3.setText(_translate("MainWindow", "Valor: "))
-        self.label_4.setText(_translate("MainWindow", "Seu saldo após a transferência: "))
+        self.label.setText(_translate("MainWindow", "Número da conta: "))
+        self.label_2.setText(_translate("MainWindow", "CPF: "))
+        self.label_3.setText(_translate("MainWindow", "Login:"))
         self.pushButton.setText(_translate("MainWindow", "Voltar"))
+        self.pushButton_2.setText(_translate("MainWindow", "Entrar"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Tela_transferir()
+    ui = Tela_login()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
--- Tela_Menu.py
+++ Tela_Menu.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class Tela_menu(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(640, 480)
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Tela_menu()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())

95bb1548ce2358f7359ff5f1675eaaef9d927c5f
Valderlandia-Macedo <valderlandiamacedo@hotmail.com>
2021-05-07 22:12:51 -0300

    Organizando os codigos em pastas para facilitar a codificação

--- Back-End/Multitela.py
+++ Back-End/Multitela.py
@@ -10,6 +10,7 @@ from Tela_Menu import Tela_menu
 from Tela_Transferir import Tela_transferir
 from Tela_Usuario import Tela_usuario
 from Tela_Login import Tela_login
+from Tela_Saque import Tela_Saque
 
 class Ui_Main(QtWidgets.QWidget):
 
